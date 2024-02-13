61146ad9e4af502cd955271353c551a9194cd8de
Tulasi Karnati <karnatitulasi2099@gmail.com>
2024-02-13 16:09:42 +0000

    catalogue

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,13 +1,4 @@
-# bucket = "daws94t-state-dev"
-# key    = "catalogue-dev"
-# region = "us-east-1"
-# dynamodb_table = "daws94t-locking-dev"
-
-terraform {
-  backend "s3" {
-    bucket = "daws94t-state-dev"
-    key    = "catalogue-dev"
-    region = "us-east-1"
-    dynamodb_table = "daws94t-locking-dev"
-  }
-}
+bucket = "daws94t-state-dev"
+key    = "catalogue-dev"
+region = "us-east-1"
+dynamodb_table = "daws94t-locking-dev"
